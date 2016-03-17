f5fb7c1693b20f694459d5b5dca840d201372d2e
Jose Chery <Jcareergoals@hotmail.com>
2016-03-17 02:45:59 -0400

    installed dependencies, created mock landing page, included background & content images in static folder

--- server.js
+++ server.js
@@ -10,12 +10,28 @@ app.use(bodyParser.urlencoded());
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-// set mongoose schema
-// create model using schema
-// get schema at to local model variable
+mongoose.connect('mongodb://localhost/the_pack'); 
 
-//create routes to end up in locals folder 
+var AnimalSchema = new mongoose.Schema({
+	name: String,
+	gender: String,
+	age: Number,
+	description: String
+}); 
+mongoose.model('rabbit', AnimalSchema); 
+var rabbit = mongoose.model('rabbit');
 
+// to be delegated to routes folder
+app.get('/', function(req, res){
+	rabbit.find({},function(err, animal){
+		if(err){
+			console.log(err);
+		} else {
+			// console.log(animal);
+			res.render('index', animal); 
+		}
+	}); 
+});
 
 app.listen(8888, function(){
 	console.log('Listening at port: 8888');
